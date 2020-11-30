ad4be43d868347c58dbb5679b915f872d89f65a6
isabellabond <ibond713@gmail.com>
2020-11-30 11:47:36 -0500

    While

--- src/main/java/plc/compiler/Generator.java
+++ src/main/java/plc/compiler/Generator.java
@@ -118,8 +118,20 @@ public final class Generator implements Ast.Visitor<Void> {
 
     @Override
     public Void visit(Ast.Statement.While ast) {
-
-        // TODO:  Generate Java to handle While node.
+        print("while (", ast.getCondition(), ") {");
+        indent++;
+        newline(indent);
+        int count = 0;
+        while(count < ast.getStatements().size()){
+            visit(ast.getStatements().get(count));
+            if(count != ast.getStatements().size()-1){
+                newline(indent);
+            }
+            count++;
+        }
+        indent--;
+        newline(indent);
+        print("}");
 
         return null;
     }
